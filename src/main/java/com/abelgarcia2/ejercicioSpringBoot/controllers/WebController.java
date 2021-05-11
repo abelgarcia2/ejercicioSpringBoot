718276e256bce7adb188b0bec66f63749e577f5c
abelgarcia2 <abelgg2000@gmail.com>
2021-05-11 17:34:48 +0200

    Subo codigo para completar feature #1

--- src/main/java/com/abelgarcia2/ejercicioSpringBoot/controllers/WebController.java
+++ src/main/java/com/abelgarcia2/ejercicioSpringBoot/controllers/WebController.java
@@ -1,8 +1,24 @@
 package com.abelgarcia2.ejercicioSpringBoot.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
-    
+
+    @GetMapping("/contar/{palabra}")
+    public String contarConsonantesVocales(@PathVariable String palabra) {
+        char[] troceada = palabra.toCharArray();
+        int vocales = 0, consonantes = 0;
+
+        for (char a : troceada) {
+            if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u') {
+                vocales++;
+            } else {
+                consonantes++;
+            }
+        }
+        return "La palabra " + palabra + " tiene " + vocales + " vocales y " + consonantes + " consonantes.";
+    }
 }
