b01da1b7516218778f2babc13a44624978f0c083
abelgarcia2 <abelgg2000@gmail.com>
2021-05-14 14:36:19 +0200

    Mejorada salida de feature #5

--- src/main/java/com/abelgarcia2/ejercicioSpringBoot/controllers/WebController.java
+++ src/main/java/com/abelgarcia2/ejercicioSpringBoot/controllers/WebController.java
@@ -1,11 +1,9 @@
 package com.abelgarcia2.ejercicioSpringBoot.controllers;
 
 import com.abelgarcia2.ejercicioSpringBoot.services.MascotaBDService;
-import com.abelgarcia2.ejercicioSpringBoot.services.TranslatedService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,17 +12,8 @@ public class WebController {
     @Autowired
     MascotaBDService mascotaBDService;
 
-    @Autowired
-    TranslatedService translatedService;
-
     @GetMapping("/listar")
     public String listarMascotas() {
         return mascotaBDService.obtenerMascotas().toString();
-    }
-
-    @GetMapping("/traduce/{palabra}")
-    public String traduce(@PathVariable String palabra) {
-        String traduccion = translatedService.traduce(palabra);
-        return traduccion;
     }
 }
